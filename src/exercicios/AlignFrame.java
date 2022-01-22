45cc65f3d8d4a9ed12128191e61964917b091081
Rafael Souto da Silva <rsowtto@gmail.com>
2022-01-21 23:34:35 -0300

    Primeiro exercício feito com sucesso. Se trata de uma GUI com GridLayout de 1 linha e 4 colunas, este layout de Grade está associado ao JFrame. E há também 4 JPanels's que usam gerenciadores FlowLayout para anexar os componentes Swing. Os JPanels com os componentes Swing são anexados ao JFrame. Não apresenta funcionalidades, serve apenas como exercício de criação de GUI's mais complexas.

---
+++ src/exercicios/AlignFrame.java
@@ -0,0 +1,96 @@
+package exercicios;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class AlignFrame extends JFrame
+{
+	private JCheckBox box1, box2;
+	private JLabel labelX, labelY;
+	private JTextField textFieldX, textFieldY;
+	private JButton buttonOk, buttonCancel, buttonHelp;
+	private FlowLayout layoutFlow;
+	private GridLayout layoutGrid;
+	private BorderLayout layoutBorder;
+	private JPanel panelFlow, panelGrid, panelBorder, panelBorder2, panelBorder3, panelBorder4;
+	
+	AlignFrame()
+	{
+		super("Alinhamento");
+		
+		this.layoutGrid = new GridLayout(1, 4);
+		this.layoutBorder = new BorderLayout();
+		
+		this.box1 = new JCheckBox("1ª checkBox");
+		this.box2 = new JCheckBox("2ª checkBox");
+		
+		this.labelX = new JLabel("Label X");
+		this.labelY = new JLabel("Label Y");
+		
+		this.textFieldX = new JTextField("8", 5);
+		this.textFieldY = new JTextField("8", 5);
+		
+		this.buttonOk = new JButton("Ok");
+		this.buttonOk.setSize(80, 25);
+		this.buttonOk.setPreferredSize(buttonOk.getSize());
+		
+		this.buttonCancel = new JButton("Cancel");
+		this.buttonCancel.setSize(80, 25);
+		this.buttonCancel.setPreferredSize(buttonCancel.getSize());
+		
+		this.buttonHelp = new JButton("Help");
+		this.buttonHelp.setSize(80, 25);
+		this.buttonHelp.setPreferredSize(buttonHelp.getSize());
+		
+		this.panelBorder = new JPanel();
+		this.panelBorder2 = new JPanel();
+		this.panelBorder3 = new JPanel();
+		this.panelBorder4 = new JPanel();
+		
+		this.setLayout(layoutGrid);
+		this.panelBorder.setLayout(new FlowLayout());
+		this.panelBorder2.setLayout(new FlowLayout());
+		this.panelBorder3.setLayout(new FlowLayout());
+		this.panelBorder4.setLayout(new FlowLayout());
+		
+		this.panelBorder.add(box1, BorderLayout.BEFORE_FIRST_LINE);
+		this.panelBorder.add(box2, BorderLayout.BEFORE_LINE_BEGINS);
+		this.add(panelBorder);
+		
+		this.panelBorder2.add(labelX, BorderLayout.BEFORE_FIRST_LINE);
+		this.panelBorder2.add(labelY, BorderLayout.BEFORE_LINE_BEGINS);
+		this.add(panelBorder2);
+		
+		this.panelBorder3.add(textFieldX, BorderLayout.BEFORE_FIRST_LINE);
+		this.panelBorder3.add(textFieldY, BorderLayout.BEFORE_LINE_BEGINS);
+		this.add(panelBorder3);
+		
+		this.panelBorder4.add(buttonOk, BorderLayout.BEFORE_FIRST_LINE);
+		this.panelBorder4.add(buttonCancel, BorderLayout.BEFORE_LINE_BEGINS);
+		this.panelBorder4.add(buttonHelp, BorderLayout.AFTER_LAST_LINE);
+		this.add(panelBorder4);
+		
+		this.initGUI();
+	}
+	
+	 void initGUI()
+	{
+		this.setDefaultCloseOperation(AlignFrame.EXIT_ON_CLOSE);
+		this.setSize(380, 150);
+//		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	 
+	 
+	 public static void main(String[] args) {
+		
+		 new AlignFrame();
+	}
+}
